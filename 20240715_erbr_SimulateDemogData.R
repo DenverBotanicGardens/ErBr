0663373579950c637ff60ab75cd3c6cd51824042
April Goebl <april.goebl@colorado.edu>
2024-07-15 15:43:31 -0600

    Update 20240715_erbr_SimulateDemogData.R

--- 20240715_erbr_SimulateDemogData.R
+++ 20240715_erbr_SimulateDemogData.R
@@ -24,7 +24,7 @@ clim32yr <- read.csv("Manuscript/erbr_climData3seas32yr_221114.csv", header=TRUE
 
 
 
-## A. First, for a set number of years (lets say 30), simulate climate variables for each year. 
+## 1. First, for a set number of years (lets say 30), simulate climate variables for each year. 
 ## What I would do is use the real data from the study to choose sets of annual data values for the set of climate variables.
 num.yrs <- 30 #Assign number of years to simulate climate data for 
 
@@ -46,7 +46,7 @@ for (cc in 1:length(1:num.yrs)) {
 
 
 
-## B. Then, use the SSD from a mean matrix to pick a set of plant starting sizes: 
+## 2. Then, use the SSD from a mean matrix to pick a set of plant starting sizes: 
 ## you can actually just use these as a set of multinomial probabilities for the starting size of each plant that is then simulated.
 
 ## Obtain stable stage structure; modified from some of Dan's code 
@@ -69,7 +69,7 @@ N.vecStart=N.startNum * mnees$stable.stage
 
 
 
-## C. for 1 to the number of starting plants (i would make this considerably larger than you think you want to have), you then do the following for each year of time:
+## 3. for 1 to the number of starting plants (i would make this considerably larger than you think you want to have), you then do the following for each year of time:
   ## a. in the first year, figure out probs of survival, growth, and reproduction for that year, given the fns for these vital rates, and the climate. 
      ## In all cases, you'll want to make this a monte carlo: for example, you pick a random chance of survival from the prob of survival, choose one new size from the distribution of possible new sizes, etc. and, store the number of new seedlings produced that are predicted to be seen in the new year.
 
@@ -201,4 +201,4 @@ lam.out.template[bb,2] <- Re(eigen(mx)$values[1])         #Calculate & store lam
 
 
 
-## D. After doing this, you would go back to the number of new plants in each year: for these, do the same approach as with the starting plants, but starting in the year they are born, and then simulate them going forward. I would use NA for the size in the years before they are 'born'.
+## 4. After doing this, you would go back to the number of new plants in each year: for these, do the same approach as with the starting plants, but starting in the year they are born, and then simulate them going forward. I would use NA for the size in the years before they are 'born'.
