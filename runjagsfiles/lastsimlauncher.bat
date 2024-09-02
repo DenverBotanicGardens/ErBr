e98e3a2dc3681c0f419bfafce9fb8e9588dbd293
April Goebl <april.goebl@colorado.edu>
2024-09-01 23:17:59 -0600

    Files made when running bg. Delete later

---
+++ runjagsfiles/lastsimlauncher.bat
@@ -0,0 +1,3 @@
+"C:/Program Files/JAGS/JAGS-4.3.1/x64/bin/jags-terminal.exe" < sim.3/script.cmd > sim.3/jagsoutput.txt 2>&1 
+"C:/Users/april.goebl/AppData/Local/R/win-library/4.2/runjags/xgrid/beep.bat"
+ 
