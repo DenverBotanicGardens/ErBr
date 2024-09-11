a4d79188af0652d17af75099fccff7abddd84ae1
April Goebl <april.goebl@colorado.edu>
2024-09-11 11:17:31 -0600

    Update erbr_2lmerStepsToJAGS_SIMdata_20240908.R

--- erbr_2lmerStepsToJAGS_SIMdata_20240908.R
+++ erbr_2lmerStepsToJAGS_SIMdata_20240908.R
@@ -36,12 +36,12 @@ library(plotrix)
 
 
 
+old <- Sys.time() # get start time
 
 ## LOOP OVER DATASETS -----------------------------------------------------------------------------
 n.datset <- 10
 for (dd in 1:n.datset) {
 
-  old <- Sys.time() # get start time
 
 ## LOAD DATA --------------------------------------------------------------------------------------
 #dats <- read.csv("20240904_erbr_SimDat20yrR2_Format4JAGS.csv", header = TRUE)
@@ -51,7 +51,7 @@ for (dd in 1:n.datset) {
 
 ## ** ASSIGN NAME VARIABLE FOR DESIRED DATASETS **
 date <- as.character("20240910")
-name <- as.character("SimDat20yrHiGrLH.Miss.")
+name <- as.character("SimDat50yrHiGrLH.Miss.")
 dats <- read.csv(file=paste(date,"_erbr_", name, dd, ".Format4JAGS", ".csv", sep=""), header=TRUE)
 ## ------------------------------------------------------------------------------------------------
 
