ccd61c5fa95b51718800fdac027140a724320f04
April Goebl <april.goebl@colorado.edu>
2024-10-12 15:13:36 -0600

    Updates

--- erbr_3JAGSmodBest_20241010.R
+++ erbr_3JAGSmodBest_20241010.R
@@ -4,28 +4,29 @@ model{
 # In the BUGS/JAGS language we must use an explicit for loop:
 
 #########################################################################
-## Doing one loop to generate the estimated sz for each plt in each yr w a survival data point (all these have sz, except the surv=0 yrs) 
-## This loop also estimates the survival to each yr: for unobserved yrs, this includes the chances of surv each unobserved yr in the past 
-## Cases in this loop are pairs of observation yrs, that can include yrs without any observations 
-for(i in 1:Ncases){
+  ## Doing one loop to generate the estimated size for each plant in each year with a survival data point (all these have size, except the survival=0 years) 
+  ## This loop also estimates the survival to each year: for unobserved years, this includes the chances of survival each unobserved year in the past 
+  ## Cases in this loop are pairs of observation years, that can include years without any observations 
+  for(i in 1:Ncases){
   
-  ## Do first lag, which must use the starting size (RosNew)
-  regression_mean[goodrows[i]-lagvals[i]+1] <- exp(grwth_intercept + grwth_RosCoef*log(RosNew[goodrows[i]-lagvals[i]]) + grwth_TempFallCoef*TempFall[goodrows[i]-lagvals[i]+1] 
+   ## Do first lag, which must use the starting size (RosNew)
+   regression_mean[goodrows[i]-lagvals[i]+1] <- exp(grwth_intercept + grwth_RosCoef*log(RosNew[goodrows[i]-lagvals[i]]) + grwth_TempFallCoef*TempFall[goodrows[i]-lagvals[i]+1] 
                                                + grwth_TempSummerCoef*TempSummer[goodrows[i]-lagvals[i]+1] + grwth_TempWinterCoef*TempWinter[goodrows[i]-lagvals[i]+1]
                                                + grwth_PptFallCoef*PptFall[goodrows[i]-lagvals[i]+1] + grwth_PptSummerCoef*PptSummer[goodrows[i]-lagvals[i]+1]
                                                + grwth_PptWinterCoef*PptWinter[goodrows[i]-lagvals[i]+1] + grwth_Transect_randomeffect[TransectNew.num[goodrows[i]-lagvals[i]]]) 
       
-  r.growth[goodrows[i]-lagvals[i]+1] <- exp(grwthvar_intercept + grwthvar_RosCoef*log(RosNew[goodrows[i]-lagvals[i]])) 
+   r.growth[goodrows[i]-lagvals[i]+1] <- exp(grwthvar_intercept + grwthvar_RosCoef*log(RosNew[goodrows[i]-lagvals[i]])) 
 
-  ## Survival prob, based on the last years observed size 
-  Surv_mu[goodrows[i]-lagvals[i]+1] <- 1/(1+exp(-(surv_intercept + surv_RosCoef*log(RosNew[goodrows[i]-lagvals[i]]) + surv_PptWinterCoef*PptWinter[goodrows[i]-lagvals[i]+1] 
+   ## Survival probability, based on the last years observed size 
+   Surv_mu[goodrows[i]-lagvals[i]+1] <- 1/(1+exp(-(surv_intercept + surv_RosCoef*log(RosNew[goodrows[i]-lagvals[i]]) + surv_PptWinterCoef*PptWinter[goodrows[i]-lagvals[i]+1] 
                                        + surv_TempWinterCoef*TempWinter[goodrows[i]-lagvals[i]+1] + surv_TempFallCoef*TempFall[goodrows[i]-lagvals[i]+1] 
                                        + surv_TempSummerCoef*TempSummer[goodrows[i]-lagvals[i]+1] + surv_Transect_randomeffect[TransectNew.num[goodrows[i]-lagvals[i]]])))
 
      
-  ## This is the loop of remaining lagged yrs for this row i: note that the : operator is diff in jags than r & can't be decreasing, hence the use of negative lag: also, if lag=1, then this will be from 0 to -1, & the loop will be skipped 
-  ## Survival rates are based on the inferred previous year size, since it was not observed 
-  for (j in (goodrows[i]-lagvals[i]+1):(goodrows[i]-1)) { 
+    ## This is the loop of remaining lagged years for this row i: note that the : operator is different in jags than R & can't be decreasing, hence the use of negative lag: 
+    ## also, if lag=1, then this will be from 0 to -1, & the loop will be skipped 
+    ## Survival rates are based on the inferred previous year size, since it was not observed 
+    for (j in (goodrows[i]-lagvals[i]+1):(goodrows[i]-1)) { 
     
      regression_mean[j+1] <- exp(grwth_intercept + grwth_RosCoef*log(regression_mean[j]) + grwth_TempFallCoef*TempFall[j+1] 
                              + grwth_TempSummerCoef*TempSummer[j+1] + grwth_TempWinterCoef*TempWinter[j+1]
@@ -43,10 +44,10 @@ for(i in 1:Ncases){
 #####################################################################
   
   #####################################################################
-  ## A loop that makes each reproyesno and repro amount estimate for years with observed sizes
+  ## A loop that makes each reproduction yes/no and reproduction amount estimate for years with observed sizes
   for(i in 1:Ndirectszcases){
     
-    ## Do the comparison for the repro yes/no data in this loop, as all observations either reproduced or not
+    ## Do the comparison for the reproduction yes/no data in this loop, as all observations either reproduced or not
     InflYesNo[rows.w.sz[i]] ~ dbern(repro_prob[rows.w.sz[i]])
     
     logit(repro_prob[rows.w.sz[i]]) <- reproyesno_intercept + reproyesno_RosCoef*log(RosNew[rows.w.sz[i]]) + reproyesno_TempFallCoef*TempFall[rows.w.sz[i]] 
@@ -58,14 +59,15 @@ for(i in 1:Ncases){
                                   + repro_PptSummerCoef*PptSummer[rows.w.sz[i]] + repro_TempWinterCoef*TempWinter[rows.w.sz[i]] 
                                   + repro_TempFallCoef*TempFall[rows.w.sz[i]] + repro_TempSummerCoef*TempSummer[rows.w.sz[i]] 
                                   + repro_Transect_randomeffect[TransectNew.num[rows.w.sz[i]]])
-                                            
-    ## Inflors is the either observed or predicted infl num to be used to make a tot repro estimate to fit predicted infls to new plts. For rows w measured infls, this is just the observed 
+    
+    ## Inflorescences is either the observed or predicted inflorescence number to be used to make a total reproduction estimates to fit predicted inflorescences to new plants
+    ## For rows with measured inflorescences, this is just the observed 
     inflors[rows.w.sz[i]] = InflNew[rows.w.sz[i]]
   }
   #####################################################################
   
   #####################################################################
-  ## Do the same for repro estimates, for years without an observation, so based on inferred size
+  ## Do the same for reproduction estimates, for years without an observation, so based on inferred size
   for(i in 1:Nindirectszcases){
 
     logit(repro_prob[rows.wo.sz[i]]) <- reproyesno_intercept + reproyesno_RosCoef*log(regression_mean[rows.wo.sz[i]]) + reproyesno_TempFallCoef*TempFall[rows.wo.sz[i]] 
@@ -77,16 +79,16 @@ for(i in 1:Ncases){
                                    + repro_PptSummerCoef*PptSummer[rows.wo.sz[i]] + repro_TempWinterCoef*TempWinter[rows.wo.sz[i]] 
                                    + repro_TempFallCoef*TempFall[rows.wo.sz[i]] + repro_TempSummerCoef*TempSummer[rows.wo.sz[i]]
                                    + repro_Transect_randomeffect[TransectNew.num[rows.wo.sz[i]]])
-     
-    ## Inflors is either the observed or predicted infl num to be used to make a tot repro estimate to fit predicted infls to new plts. For rows w/o measured infls, this is just the predicted num, accounting for surv & reproyesno
-    ## Max statement enforces that if a plt must have been alive, its estimated infs should be counted in the tot for estimating new plts
+    ## Inflorescences is either the observed or predicted inflorescence number to be used to make a total reproduction estimates to fit predicted inflorescences to new plants 
+    ## For rows without measured inflorescences, this is just the predicted number, accounting for survival & reproduction yes/no
+    ## Max statement enforces that if a plant must have been alive, its estimated inforescences should be counted in the total for estimating new plants 
     inflors[rows.wo.sz[i]] = max(Surv_mu[rows.wo.sz[i]], rows.wo.sz.alive[i])*repro_prob[rows.wo.sz[i]]*repro_amount[rows.wo.sz[i]]
   }
   #####################################################################
   
   
 #####################################################################
-  ## Loop over only rows with >0 inflors, to compare estimated inflors to observed
+  ## Only loop over rows with >0 inflorescences, to compare estimated inflorescences to observed
   for(i in 1:Nrows.w.inflors) {
  
    p.infls[i] <- r.infls/(r.infls + repro_amount[rows.w.inflors[i]])
@@ -120,7 +122,8 @@ for(i in 1:Ncases){
   
   
 #####################################################################
-  ## This loop is getting the predicted num of tot infls for each transect/year combo, & fitting to num of new plts a yr later. The inprod & the 1st logical are making 0,1 for whether a given line is part of each trans-yr combo
+  ## This loop is getting the predicted number of total inflorescences for each transect-year combo, & fitting to number of new plants a year later 
+  ## The inprod & the 1st logical are making 0,1 for whether a given line is part of each transect-year combo
   for (i in 1:newPltlines){
 
        pred.tot.inflors[i] = inprod(yrtranscombo==newplt.yrtranscombo[i], inflors)
@@ -139,10 +142,8 @@ for(i in 1:Ncases){
 ########################################
 ########################################    
 ## These lines give the prior distributions for the parameters to be estimated
- # r_disp ~ dgamma(0.01, 0.01) # this is the dispersion for all cases, as is standard
-  r_disp ~ dunif(0,50) ## ** REVIEWER ASKED FOR THIS TO BE REMOVED AS IT'S NOT USED **
+  #r_disp ~ dunif(0,50) 
 	# Note: the prior for the dispersion parameter k is quite important for convergence
-	# [A DuMouchel prior may be better than a Gamma prior]
 
 ## GROWTH  
 grwth_intercept ~ dunif(-5,5) 
@@ -179,7 +180,7 @@ for(TransectNew.num_iterator in 1:12){
 surv_Transect_precision ~ dunif(0,1) 
 
 
-## PROB OF REPRO
+## PROBABILITY OF REPRODUCTION
 reproyesno_intercept ~  dnorm(0, 10^-6)
 reproyesno_RosCoef ~  dnorm(0, 10^-6)
 reproyesno_PptFallCoef ~  dnorm(0, 10^-6)
@@ -188,13 +189,13 @@ reproyesno_TempFallCoef ~  dnorm(0, 10^-6)
 reproyesno_TempSummerCoef ~  dnorm(0, 10^-6)
 reproyesno_TempWinterCoef ~  dnorm(0, 10^-6)
 
-## Repro prob transect random effects
+## Reproduction probability transect random effects
 for(TransectNew.num_iterator in 1:12){
   reproyesno_Transect_randomeffect[TransectNew.num_iterator] ~ dnorm(0, reproyesno_Transect_precision) }
 reproyesno_Transect_precision ~ dunif(0,1)
 
 
-# AMT OF REPRO
+# AMOUNT OF REPRODUCTION
 r.infls ~ dunif(0,10) 
 repro_intercept ~ dunif(-5,5) 
 repro_RosCoef ~ dunif(0.5,1.5) 
@@ -204,13 +205,13 @@ repro_TempWinterCoef ~ dnorm(0, 10^-6)
 repro_TempFallCoef ~ dnorm(0, 10^-6)  
 repro_TempSummerCoef ~ dnorm(0, 10^-6)   
 
-## Repro amt transect random effects
+## Reproduction amount transect random effects
 for(TransectNew.num_iterator in 1:12){
   repro_Transect_randomeffect[TransectNew.num_iterator] ~ dnorm(0, repro_Transect_precision) }
 repro_Transect_precision ~ dunif(0,1)
 
 
-## Neg binomial variance for predicted inflorescence number
+## Negative binomial variance for predicted inflorescence number
 inflors_precision ~ dgamma(0.001, 0.001)
 var.inflors = 1/inflors_precision
 
@@ -224,9 +225,8 @@ r.newplts ~ dgamma(0.01,0.01)
 } # end of model specification 
 
 
-## ADD BACK TO MONITOR (optional): dic
 # These lines are hooks to be read by runjags (they are ignored by JAGS):
-#monitor# deviance,grwth_Transect_randomeffect,surv_Transect_randomeffect,reproyesno_Transect_randomeffect,repro_Transect_randomeffect,grwth_intercept,grwth_RosCoef,grwth_TempFallCoef,grwth_TempSummerCoef,grwth_TempWinterCoef,grwth_PptFallCoef,grwth_PptSummerCoef,grwth_PptWinterCoef,grwthvar_intercept,grwthvar_RosCoef,surv_intercept,surv_RosCoef,surv_PptWinterCoef,surv_TempFallCoef,surv_TempSummerCoef,surv_TempWinterCoef,reproyesno_intercept,reproyesno_RosCoef,reproyesno_PptFallCoef,reproyesno_PptSummerCoef,reproyesno_TempFallCoef,reproyesno_TempSummerCoef,reproyesno_TempWinterCoef,repro_intercept,repro_RosCoef,repro_PptFallCoef,repro_PptSummerCoef,repro_TempWinterCoef,repro_TempFallCoef,repro_TempSummerCoef,newplt_intercept,r.infls,r.newplts,repro_Transect_precision,reproyesno_Transect_precision,surv_Transect_precision,grwth_Transect_precision
+#monitor# deviance,grwth_Transect_randomeffect,surv_Transect_randomeffect,reproyesno_Transect_randomeffect,repro_Transect_randomeffect,repro_Transect_precision,reproyesno_Transect_precision,surv_Transect_precision,grwth_Transect_precision,grwth_intercept,grwth_RosCoef,grwth_TempFallCoef,grwth_TempSummerCoef,grwth_TempWinterCoef,grwth_PptFallCoef,grwth_PptSummerCoef,grwth_PptWinterCoef,grwthvar_intercept,grwthvar_RosCoef,surv_intercept,surv_RosCoef,surv_PptWinterCoef,surv_TempFallCoef,surv_TempSummerCoef,surv_TempWinterCoef,reproyesno_intercept,reproyesno_RosCoef,reproyesno_PptFallCoef,reproyesno_PptSummerCoef,reproyesno_TempFallCoef,reproyesno_TempSummerCoef,reproyesno_TempWinterCoef,repro_intercept,repro_RosCoef,repro_PptFallCoef,repro_PptSummerCoef,repro_TempWinterCoef,repro_TempFallCoef,repro_TempSummerCoef,newplt_intercept,r.infls,r.newplts
 #modules# glm on
 #response# RosNew
 #residual# regression_residual
